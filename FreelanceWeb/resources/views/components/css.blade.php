fcc9e203b2727c3a2a7bd631c0a0650861626189
Saaria Zahid <102374986+Saaria-Zahid@users.noreply.github.com>
2024-07-05 15:01:58 +0500

    Delete

--- FreelanceWeb/resources/views/components/css.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-
--- FreelanceWeb/resources/views/components/select.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['disabled' => false])
-
-<select {!! $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) !!}>
-    {{ $slot }}
-</select>
